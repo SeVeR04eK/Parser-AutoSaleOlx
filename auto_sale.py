9d66a2c1d99e943c326bc67de2535b46be467e55
Andrii_Severyn <andrej.chees.bs@gmail.com>
2026-01-15 23:51:10 +0100

    add full process of posting ad on olx, chnaged structure of auto_sale.py by making it more readable and usefull for main and separating code into functions, also replase sleep with wait class. Change parser.py for saving images in folder foto which is now added to .gitignore

--- auto_sale.py
+++ auto_sale.py
@@ -1,7 +1,92 @@
 from stealth_driver import get_driver
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = get_driver()
-driver.get("https://www.olx.pl/")
-time.sleep(15)
-driver.quit()
+wait = WebDriverWait(driver, 20)
+def open_olx():
+    driver.get("https://www.olx.pl/adding/")
+def post_ad():
+    # ---------- Title ----------
+    title_input = wait.until(
+        EC.element_to_be_clickable((By.NAME, "title"))
+    )
+    title_input.send_keys("Czajnik elektryczny kamille")
+
+    # ---------- Description ----------
+    desc = wait.until(
+        EC.presence_of_element_located((By.ID, "description"))
+    )
+    desc.send_keys("Telefon w bardzo dobrym stanie, działa perfekcyjnie.")
+
+    # ---------- Condition (Stan) ----------
+    fields = WebDriverWait(driver, 20).until(
+        lambda d: d.find_elements(By.CSS_SELECTOR, "input.n-textinput-input[role='combobox']")
+                  if len(d.find_elements(By.CSS_SELECTOR, "input.n-textinput-input[role='combobox']")) >= 2
+                  else False
+    )
+
+    fields[1].click()
+
+    option = wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//button[@role='option' and contains(., 'Nowe')]")
+        )
+    )
+    option.click()
+
+    # ---------- Price ----------
+    price = wait.until(
+        EC.element_to_be_clickable((By.NAME, "parameters.price.price"))
+    )
+    price.send_keys("1200")
+
+    # ---------- Delivery: expand size L ----------
+    toggle = wait.until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//div[@data-testid='band-L-toggle']/ancestor::header")
+        )
+    )
+    toggle.click()
+
+    # ---------- Delivery: select all carriers ----------
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//input[@aria-label='INPOST package size L']")
+        )
+    )
+
+    driver.find_element(
+        By.XPATH, "//input[@aria-label='INPOST package size L']"
+    ).click()
+
+    driver.find_element(
+        By.XPATH, "//input[@aria-label='RUCH package size L']"
+    ).click()
+
+    driver.find_element(
+        By.XPATH, "//input[@aria-label='POCZTA package size L']"
+    ).click()
+
+    # ---------- Courier delivery (radio buttons) ----------
+    radios = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, "input[type='radio'][data-testid^='radio-']")
+        )
+    )
+    driver.execute_script("arguments[0].click();", radios[1])
+
+    # ---------- Image upload ----------
+    photo_input = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file']"))
+    )
+    photo_input.send_keys(
+        r"C:\Programming\Projects (mixed)\Parser-AutoPostOLX\foto\foto1.png"
+    )
+
+    # Keep browser open for manual review
+    wait.until(lambda d: False)
+
+    # submit_btn = driver.find_element(By.CSS_SELECTOR, "button[data-testid='submit-button']")
+    # submit_btn.click()
--- parser.py
+++ parser.py
@@ -3,16 +3,36 @@ import html
 from bs4 import BeautifulSoup
 from typing import Generator, Dict, Any
 from time import sleep
+import os
 
 class Parser:
     url_base = "https://www.kamille.pl/webapi/front/pl_PL/products/PLN/list/"
+
     @classmethod
     def get_json(cls) -> Generator[Dict[str, Any], None, None]:
         # Getting JSON
-        for i in range(1, 10001):
+        for i in range(3000, 10001):
             url_ = cls.url_base + str(i)
             response = requests.get(url_)
             yield response.json()
+
+    @staticmethod
+    def save_image(img_url, folder="foto", filename="foto1.png"):
+        os.makedirs(folder, exist_ok=True)
+
+        save_path = os.path.join(folder, filename)
+
+        if os.path.exists(save_path):
+            os.remove(save_path)
+
+        response = requests.get(img_url)
+        with open(save_path, "wb") as f:
+            f.write(response.content)
+
+        print("Saved:", save_path)
+        return save_path
+
+
     @staticmethod
     def get_data():
         for data in Parser.get_json():
@@ -23,6 +43,7 @@ class Parser:
                 #Saving data in variables
                 name = data["list"][0]["name"]
                 img = "https://www.kamille.pl/userdata/public/gfx/"+data["list"][0]["main_image_filename"]
+                Parser.save_image(img)
 
                 #Desc
                 raw = data["list"][0]["description"]
@@ -43,9 +64,11 @@ class Parser:
                 middle_price = (price_origin + price_origin_net) / 2
                 my_price = round((price_origin + middle_price) / 2, 2)
 
-                yield name, img, description, my_price
+                yield name, description, my_price
 
 def main():
     parser = Parser()
     for item in parser.get_data():
         print(item,"\n")
+
+main()
